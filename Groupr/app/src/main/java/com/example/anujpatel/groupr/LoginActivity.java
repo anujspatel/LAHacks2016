256b5481b66285de5b1ec8f5c0dfb5141c547f33
Anuj Patel <anujpatel@berkeley.edu>
2016-04-30 21:10:23 -0700

    added sign up page

--- Groupr/app/src/main/java/com/example/anujpatel/groupr/LoginActivity.java
+++ Groupr/app/src/main/java/com/example/anujpatel/groupr/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.anujpatel.groupr;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -28,6 +29,8 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,8 +50,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
      */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+    private static final ArrayList<String> DUMMY_CREDENTIALS = new ArrayList<String>(){
+            //"foo@example.com:hello", "bar@example.com:world"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -60,6 +63,18 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+    private boolean sendToSignUp;
+    private boolean sendToDiscovery;
+
+    public void sendSomewhere(View view) {
+        if (sendToDiscovery) {
+            Intent intent = new Intent(this, Discovery.class);
+            startActivity(intent);
+        } else if (sendToSignUp) {
+            Intent intent = new Intent(this, FinishSignUp.class);
+            startActivity(intent);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,6 +101,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             @Override
             public void onClick(View view) {
                 attemptLogin();
+                sendSomewhere(view);
             }
         });
 
@@ -185,6 +201,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // perform the user login attempt.
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
+            if (DUMMY_CREDENTIALS.contains(email + ":" + password)) {
+                sendToDiscovery = true;
+            } else {
+                sendToSignUp = true;
+            }
             mAuthTask.execute((Void) null);
         }
     }
@@ -317,11 +338,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 String[] pieces = credential.split(":");
                 if (pieces[0].equals(mEmail)) {
                     // Account exists, return true if the password matches.
+                    sendToDiscovery = true;
                     return pieces[1].equals(mPassword);
                 }
             }
 
+            sendToSignUp = true;
             // TODO: register the new account here.
+            DUMMY_CREDENTIALS.add(mEmail + ":" + mPassword);
             return true;
         }
 
